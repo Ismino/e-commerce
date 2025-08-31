f464d82232aab5289204082a89cff4c67361d517
Ismino <denise_cigel@hotmail.com>
2025-08-31 15:43:03 +0000

    add basic layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,4 +3,11 @@ export const metadata = { title: "Catalog", description: "DummyJSON catalog" };
 import ".globals.css";
 import Providers from "./providers";
 import CartDrawer from "@/components/CartDrawer";
+import CartButton from "@/components/CartButton";
 
+export default function RootLayout({children}: {children: React.ReactNode}) {
+    return (
+        <html 
+    )
+
+}
